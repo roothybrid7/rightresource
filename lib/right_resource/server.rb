6f3cd28a2f77d8f8af61fd6d8655aba9f3ae9c08
Satoshi Ohki <roothybrid7@gmail.com>
2010-10-25 21:38:34 +0900

    bug fix: future method: __method__ in v1.8.5

--- lib/right_resource/server.rb
+++ lib/right_resource/server.rb
@@ -1,13 +1,13 @@
 class Server < RightResource::Base
   class << self
     def settings(id, params={})
-      path = "#{resource_name}s/#{id}/#{__method__.to_s}.#{format.extension}#{query_string(params)}"
+      path = "#{resource_name}s/#{id}/settings.#{format.extension}#{query_string(params)}"
       instantiate_record(format.decode(connection.get(path)))
     end
   end
 
   def settings
-    path = "#{@resource_name}s/#{@id}/#{__method__.to_s}.#{self.class.format.extension}"
+    path = "#{@resource_name}s/#{@id}/settings.#{self.class.format.extension}"
     sub_attrs = generate_attributes(self.class.format.decode(connection.get(path)))
     self.attributes.merge! sub_attrs
     load_accessor(sub_attrs)
--- lib/right_resource/server_array.rb
+++ lib/right_resource/server_array.rb
@@ -1,19 +1,18 @@
 class ServerArray < RightResource::Base
   class << self
     def instances(id, params={})
-      path = "#{resource_name}s/#{id}/#{__method__.to_s}.#{format.extension}#{query_string(params)}"
+      path = "#{resource_name}s/#{id}/instances.#{format.extension}#{query_string(params)}"
       instantiate_collection(format.decode(connection.get(path)))
     end
   end
 
   def instances
-    path = "#{@resource_name}s/#{@id}/#{__method__.to_s}.#{self.class.format.extension}"
+    path = "#{@resource_name}s/#{@id}/instances.#{self.class.format.extension}"
     instances = self.class.instantiate_collection(self.class.format.decode(connection.get(path)))
     if instances.size > 0
       self.attributes[:active_instances] = []
       instances.each do |ins|
-        link_id = ins.resource_uid.gsub('-', '_').to_s
-        self.attributes[:active_instances] << {link_id.to_sym => ins.attributes}
+        self.attributes[:active_instances] << ins.attributes
       end
       load_accessor(:active_instances => self.attributes[:active_instances])
     end
