5b828d35d4ea6dec7631e590cda28913692f5f3f
Satoshi Ohki <roothybrid7@gmail.com>
2010-11-04 21:03:40 +0900

    add: core_ext

--- lib/right_resource/core_ext.rb
+++ lib/right_resource/core_ext.rb
@@ -1,3 +1,3 @@
 Dir["#{File.dirname(__FILE__)}/core_ext/*.rb"].sort.each do |path|
-  require "active_support/core_ext/#{File.basename(path, '.rb')}"
+  require "right_resource/core_ext/#{File.basename(path, '.rb')}"
 end
