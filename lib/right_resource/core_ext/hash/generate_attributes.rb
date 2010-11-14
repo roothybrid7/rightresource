2cab626e7cf166a62787415261d479b9b2bb31ce
Satoshi Ohki <roothybrid7@gmail.com>
2010-11-15 02:41:29 +0900

    refactor: method chain

--- lib/right_resource/core_ext/hash/generate_attributes.rb
+++ lib/right_resource/core_ext/hash/generate_attributes.rb
@@ -2,10 +2,7 @@ class Hash
   def generate_attributes
     attrs = {}
     self.each_pair {|key,value| attrs[key.to_s.gsub('-', '_').to_sym] = value}
-    if attrs
-      self.clear
-      self.merge!(attrs)
-    end
+    self.clear.merge!(attrs) if attrs
     self
   end
 end
